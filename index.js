fb949c3bb89596d5df26cb15c7401404f4296b6a
Sena Nur Özcan <1.senaozcan@gmail.com>
2022-09-14 15:13:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-/* this is a multi-line comment
-this line is not executed
-and this one too */
-console.log('Hello, world');
+// you can assign another value to this variable later
+let age = 37;
+
+// let's assign another value
+age = 38;
+
+console.log(age); //we'll get 38
+
+// you cannot assign another value to this variable
+const name = 'Misha';
+
+name = 'Petya'; //throws an error
