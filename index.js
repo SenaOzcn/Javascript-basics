3fedc7c64101e8a5b775277805ed1f3d64f37199
Sena Nur Özcan <1.senaozcan@gmail.com>
2022-09-14 19:32:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,37 +1,6 @@
-// `===` checks if values are equal
 console.log(
-  2 === 2, // true
-  2 === 3, // false
-);
-
-// `!==` works the opposite of equality
-console.log(
-  2 !== 2, // false
-  2 !== 3, // true
-);
-
-// `>` checks if the value on the left is greater than the value on the right
-console.log(
-  3 > 2, // true
-  1 > 2, // false
-  2 > 2, // false
-);
-
-// `<` checks if the value on the left is less than the value on the right
-console.log(
-  2 < 3, // true
-  2 < 1, // false
-  2 < 2, // false
-);
-
-// `>=` checks if the value on the left is greater than or equal to the value on the right
-console.log(
-  1 >= 2, // false
-  2 >= 2, // true
-);
-
-// `<=` checks if the value on the left is less than or equal to the value on the right
-console.log(
-  1 <= 2, // true
-  2 <= 2, // true
+  'b' > 'a', // true
+  'abc' > 'aba', // true
+  'sunny' > 'sun', // true
+  'hello' === 'hello', // true
 );
