be34c9c5b57ef8bd93889f7f9e4897c11f426985
Sena Nur Özcan <1.senaozcan@gmail.com>
2022-09-14 16:02:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,2 @@
-// initially the box is empty
-let box = null;
-
-// we put a gift in the box
-box = 'Birthday present';
-
-// the box is empty again
-box = null;
+// this one is undefined because no value has been set
+let value;
