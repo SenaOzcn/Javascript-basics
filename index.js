de66856dda6a4824204f6c7f5910401ff3bee1d1
Sena Nur Özcan <1.senaozcan@gmail.com>
2022-09-14 15:17:16 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
-// you can assign another value to this variable later
-let age = 37;
+// good name
+const cookiePrice = 15;
 
-// let's assign another value
-age = 38;
-
-console.log(age); //we'll get 38
-
-// you cannot assign another value to this variable
-const name = 'Misha';
-
-name = 'Petya'; //throws an error
+// bad name
+const cookie_price = 15; // some other languages use such names but in Javascript we will avoid them
+const priceof1cake = 25; // hard to see word borders
+const value = 15; // this name doesn't explain what value it is
