02acf676bb506c718dc5535676a8c730bcb86f9f
Sena Nur Özcan <1.senaozcan@gmail.com>
2022-09-14 16:22:56 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,2 @@
-let addition = 10 + 5; // addition
-console.log(addition); // 15
-
-let subtraction = 10 - 5; // subtraction
-console.log(subtraction); // 5
-
-let multiplication = 10 * 5; // multiplication
-console.log(multiplication); // 50
-
-let division = 10 / 5; // division
-console.log(division); // 2
-
-let exponentiation = 10 ** 5; // exponentiation
-console.log(exponentiation); // 100000
-
-let remainder = 16 % 7; // remainder of the division
-console.log(remainder); // 2 - subtract 7 until the result is less than 7
+let number = 345 % 10;
+console.log(number); // 5
