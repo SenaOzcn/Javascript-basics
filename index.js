5c84b5f2cb4f60ed7d41d8261ab312efe807d556
Sena Nur Özcan <1.senaozcan@gmail.com>
2022-09-14 16:08:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-// this one is undefined because no value has been set
-let value;
+const name = 'Misha';
+console.log(typeof name); // 'string'
+
+const age = 37;
+console.log(typeof age); // 'number'
+
+const isMarried = true;
+console.log(typeof isMarried); // 'boolean'
