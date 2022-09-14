11bf43c7295692547242bd0036909a247900bc24
Sena Nur Özcan <1.senaozcan@gmail.com>
2022-09-14 15:58:34 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-const isMarried = true;
-const hasDog = false;
+// initially the box is empty
+let box = null;
+
+// we put a gift in the box
+box = 'Birthday present';
+
+// the box is empty again
+box = null;
