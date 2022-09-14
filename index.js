4e27bf5e8983603ffab882ffba0cd78a95be26b6
Sena Nur Özcan <1.senaozcan@gmail.com>
2022-09-14 16:18:53 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-console.log(typeof undefined); // 'undefined'
+let addition = 10 + 5; // addition
+console.log(addition); // 15
 
-// this is a documented "bug" of Javascript
-// don't even try to understand why 'object' 😊
-console.log(typeof null); // 'object'
+let subtraction = 10 - 5; // subtraction
+console.log(subtraction); // 5
+
+let multiplication = 10 * 5; // multiplication
+console.log(multiplication); // 50
+
+let division = 10 / 5; // division
+console.log(division); // 2
+
+let exponentiation = 10 ** 5; // exponentiation
+console.log(exponentiation); // 100000
+
+let remainder = 16 % 7; // remainder of the division
+console.log(remainder); // 2 - subtract 7 until the result is less than 7
