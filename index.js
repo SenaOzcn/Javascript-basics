b2abcc5e9481b236c3a9b1912b974c8c5397499e
Sena Nur Özcan <1.senaozcan@gmail.com>
2022-09-15 14:40:39 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-function sayHello(personName) {
+// good name
+function sayHello(personName) { // describes the purpose of the function
+  console.log(`Hello, ${personName}!`)
+}
+
+// bad names
+function func(personName) { // does not explain what the function does
+  console.log(`Hello, ${personName}!`)
+}
+
+function sayhello(personName) { // is hard to read, the second word starts with a small letter
   console.log(`Hello, ${personName}!`)
 }
