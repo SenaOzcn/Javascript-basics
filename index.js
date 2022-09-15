21bd2426f593d51730affd9934e9f8c8faa91d60
Sena Nur Özcan <1.senaozcan@gmail.com>
2022-09-16 00:48:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
-function getTitle(age) {
-  if (age >= 18) {
-    // when age >= 18 exit the function
-    return 'Adult';
-  }
-  
-  // the program will get here only if the condition age >= 18 is not met
-  // so in the following code we know for sure that age < 18
-  if (age >= 10) {
-    // when age >= 10 (but < 18) exit the function
-    return 'Teenager';
-  }
-  
-  // we get here only if we have not exited the function earlier
-  // it means that age < 18 and age < 10
-  return 'Child';
+let age = 23;
+let result = '';
+
+// condition
+if (age >= 18) {
+  // value1
+  result = 'Adult';
+} else {
+  // value2
+  result = 'Not adult';
 }
+
+console.log(result); // 'Adult'
