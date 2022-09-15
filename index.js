46c431f23260756e7e0bc8bcce23a5a72ef91998
Sena Nur Özcan <1.senaozcan@gmail.com>
2022-09-16 00:38:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 if (age >= 18) {
-  console.log('Adult');
+  console.log('Adult')
 } else if (age >= 10) {
-  // will work if age < 18 and age >= 10
-  console.log('Teenager');
-} else {
-  // will work if age < 10 and age > 3
+  console.log('Teenager')
+} else if (age > 3) {
   console.log('Child');
+} else {
+  console.log('Baby');
 }
