3809f7defd04609748d79f1c4635cb7773580492
Sena Nur Özcan <1.senaozcan@gmail.com>
2022-09-15 14:51:48 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-function getRectangleSquare(a, b) {
-  const square = a * b;
-  
-  return square;
-}
+const square1 = getRectangleSquare(3, 2);
+console.log(square1); // 6
+
+const square2 = getRectangleSquare(4, 5);
+console.log(square2); // 20
