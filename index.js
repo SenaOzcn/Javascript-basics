38b8015c2e6344e8810698edbaa5d452336af354
Sena Nur Özcan <1.senaozcan@gmail.com>
2022-09-16 00:17:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
-let age = 16;
-
-console.log('Go to the shop');
-
-if (age >= 18) {
-  // this line will only be executed if `age` is at least 18
-  console.log('You can buy alcohol');
+if (isAdult) {
+  if (hasEnoughMoney) {
+    console.log('You can buy alcohol');
+  }
 }
-
-console.log('Come back home');
