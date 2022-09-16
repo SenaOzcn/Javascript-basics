0106e799d2de3cca15472bd05f80071338ba6cf0
Sena Nur Özcan <1.senaozcan@gmail.com>
2022-09-16 12:46:51 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
-const result = (age >= 18) 
-  ? 'Adult' 
-  : 'Not adult';
+for (let age = 3; age < 10; age++) {
+  // will be executed 7 times for ages 3 to 9
+  console.log(`My age is ${age}`);
+}
