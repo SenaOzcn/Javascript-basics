b5e57b31a0adf52d239bd16f592d0ee6ec681acf
Sena Nur Özcan <1.senaozcan@gmail.com>
2022-09-16 13:14:47 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// increment `i` by 10 starting from 5
-for (let i = 5; i < 30; i += 10) {
+// after each iteration `i` is increased by 3 times
+for (let i = 1; i < 30; i *= 3) {
   console.log(i);
 }
